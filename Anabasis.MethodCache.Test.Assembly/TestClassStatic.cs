f321b119d44671ddc300e8d4fa3edd7a1806b2ac
thomas.raynal <thomas.raynal@beezup.com>
2021-12-25 15:11:18 +0100

    handle static class/methods

---
+++ Anabasis.MethodCache.Test.Assembly/TestClassStatic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anabasis.MethodCache
+{
+    public static class TestClassStatic
+    {
+
+        [Cache]
+        public static string TestStaticClassMethod(int a, int b)
+        {
+            Debug.WriteLine(nameof(TestStaticClassMethod));
+
+            return $"{a + b}";
+        }
+    }
+
+    public class TestMethodStatic
+    {
+
+        [Cache]
+        public static string TestStaticMethod(int a, int b)
+        {
+            Debug.WriteLine(nameof(TestStaticMethod));
+
+            return $"{a + b}";
+        }
+
+    }
+}
---
+++ Anabasis.MethodCache.Test/MethodCacheStaticTests.cs
@@ -0,0 +1,74 @@
+﻿using Anabasis.MethodCache.Fody;
+using Fody;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anabasis.MethodCache.Test
+{
+	[NonParallelizable]
+	public class MethodCacheStaticTests
+	{
+
+		private static TestResult TestResult { get; }
+
+		static MethodCacheStaticTests()
+		{
+			var weavingTask = new ModuleWeaver();
+
+			TestResult =
+				weavingTask.ExecuteTestRun("Anabasis.MethodCache.Test.Assembly.dll", ignoreCodes: new[] { "0x80131869" }, runPeVerify: false);
+		}
+
+		[SetUp]
+		public void Setup()
+		{
+			CachingServices.KeyBuilder = new TestCacheKeyBuilder();
+			CachingServices.Backend = new TestBackend();
+		}
+
+		[Test]
+		public void ShouldTestStaticMethod()
+		{
+			dynamic instance = TestHelpers.CreateInstance<TestMethodStatic>(MethodCacheStaticTests.TestResult.Assembly, null);
+
+			var result = (string)TestResult.Assembly.GetType("Anabasis.MethodCache.TestMethodStatic")
+				.GetMethod("TestStaticMethod")
+				.Invoke(instance, new object[] { 1, 2 });
+
+
+			Assert.NotNull(result);
+
+			var hasValue = CachingServices.Backend.TryGetValue(
+				"Anabasis.MethodCache.TestMethodStatic.TestStaticMethod|a|1;b|2",
+				out string cacheValue);
+
+			Assert.True(hasValue);
+			Assert.AreEqual("3", cacheValue);
+
+		}
+
+        [Test]
+        public void ShouldTestStaticClass()
+        {
+			var result = (string)TestResult.Assembly.GetType("Anabasis.MethodCache.TestClassStatic")
+			   .GetMethod("TestStaticClassMethod")
+			   .Invoke(null, new object[] { 1, 2 });
+
+            Assert.NotNull(result);
+
+            var hasValue = CachingServices.Backend.TryGetValue(
+				"Anabasis.MethodCache.TestClassStatic.TestStaticClassMethod|a|1;b|2",
+                out string cacheValue);
+
+            Assert.True(hasValue);
+            Assert.AreEqual("3", cacheValue);
+
+        }
+
+
+    }
+}
